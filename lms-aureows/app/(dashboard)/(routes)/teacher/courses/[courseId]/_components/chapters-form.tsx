61918d8985a74e8be3618e628717bd0d4822118d
aureowebsoutions <aureowebsolutions@gmail.com>
2023-12-10 01:57:45 -0500

    chapter creating firts part

---
+++ lms-aureows/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
@@ -0,0 +1,123 @@
+"use client"
+import * as z from "zod";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { PlusCircle } from "lucide-react";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormMessage,
+} from "@/components/ui/form";
+import { cn } from "@/lib/utils";
+import { Course, Chapter } from "@prisma/client";
+import { Input } from "@/components/ui/input";
+
+
+interface ChaptersFormProps {
+    initialData: Course & {chapters: Chapter[]};
+    courseId: string;
+}
+
+const formSchema = z.object({
+    title: z.string().min(1, {
+        message: "Title is required",
+    }),
+});
+
+export const ChaptersForm =({
+    initialData,
+    courseId
+}: ChaptersFormProps) => {
+    const [isCreating, setIsCreating] = useState(false);
+    const [isUpdating, setIsUpdating] = useState(false);
+
+    const toogleCreating = () => {
+        setIsCreating((current) => !current);
+    }
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            title: "",
+        }
+    });
+
+    const { isSubmitting, isValid} = form.formState;
+
+    const router = useRouter();
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try {
+            await axios.post(`/api/courses/${courseId}/chapters`, values);
+            toast.success("Chapter Created");
+            toogleCreating();
+            router.refresh();
+        } catch {
+            toast.error("Something went wrong");
+        }
+    }
+
+    return(
+        <div className="p-4 mt-6 border rounded-md bg-slate-100">
+            <div className="flex items-center justify-between font-medium">
+                Course chapters
+                <Button onClick={toogleCreating} variant="ghost">
+                    {isCreating ? (
+                        <>Cancel</>
+                    ) : (
+                        <>
+                            <PlusCircle className="w-4 h-4 mr-2"/>
+                            Add a Chapter
+                        </>
+                    )}
+                    
+                </Button>
+            </div>
+            {isCreating && (
+                <Form {...form}>
+                    <form 
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className="mt-4 space-y-4">
+                        <FormField 
+                        control={form.control}
+                        name="title"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormControl>
+                                    <Input disabled={isSubmitting}
+                                    placeholder="e.g. 'Introduction to the course'"
+                                    {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                        />
+                        <Button disabled={!isValid || isSubmitting}
+                        type="submit">
+                            Create
+                        </Button>
+                    </form>
+                </Form>
+            )}
+            {!isCreating && (
+                <div className={cn("mt-2 text-sm",
+                !initialData.chapters.length && "italic text-slate-500")}>
+                    {!initialData.chapters.length && "No Chapters"}
+                </div>
+            )}
+            {!isCreating && (
+                <p className="mt-4 text-xs text-muted-foreground">
+                    Drag And Drop to reorder the chapters
+                </p>
+            )}
+        </div>
+    )
+}
